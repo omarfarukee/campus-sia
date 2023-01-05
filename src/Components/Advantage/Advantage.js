372c04e4a37a7ec18fe2628e3c719d2da8901740
omarfarukee <umarfaruque1920@gmail.com>
2023-01-05 21:41:44 +0600

    final push

--- src/Components/Advantage/Advantage.js
+++ src/Components/Advantage/Advantage.js
@@ -15,21 +15,21 @@ const Advantage = () => {
             <div className='flex justify-center'>
                 <div className='gap-5 mt-5 grid grid-cols-3'>
                     <div className='w-80 border rounded-2xl shadow-2xl'>
-                        <img src={img1} className='w-full w-64 rounded-2xl' alt="..." />
+                        <img src={img1} className='w-full rounded-2xl' alt="..." />
                         <div className='p-3'>
                             <h1 className='text-2xl font-bold'>Up to 100% scholarship</h1>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
                         </div>
                     </div>
                     <div className='w-80 border rounded-2xl shadow-2xl'>
-                        <img src={img2} className='w-full rounded-2xl w-64' alt="..." />
+                        <img src={img2} className='w-full rounded-2xl' alt="..." />
                         <div className='p-3'>
                             <h1 className='text-2xl font-bold'>Free cost of using Lab</h1>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
                         </div>
                     </div>
                     <div className='w-80 border rounded-2xl shadow-2xl'>
-                        <img src={img3} className='w-full rounded-2xl w-64' alt="..." />
+                        <img src={img3} className='w-full rounded-2xl' alt="..." />
                         <div className='p-3'>
                             <h1 className='text-2xl font-bold'>Monthly Allowance</h1>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
